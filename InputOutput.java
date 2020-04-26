a0781dfeff34bdc27f1506d67b6b794ccc1bc40f
isnawati <isnawati222@gmail.com>
2020-04-26 12:25:32 +0800

    Buat bInput String kedalam Variabel

--- InputOutput.java
+++ InputOutput.java
@@ -1,6 +1,9 @@
 public class InputOutput{
  public static void main(String [] args){
   Scanne scanner = new Scanner(System.in);
+  String nama;
+  System.out.print("Masukkan nama anda : ");
+  nama = scanner.nextLine();
  
  }
 }
